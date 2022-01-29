8f51cd1e71e171f4aaccd370e7958f1cc2f82c41
root <kalmasri@me.com>
2022-01-29 16:57:44 -0500

    role

--- src/controllers/PresenceUpdate.ts
+++ src/controllers/PresenceUpdate.ts
@@ -7,18 +7,22 @@ export default async function PresenceUpdate(oldPresence: Presence, newPresence:
     let isPlayingLeague = newPresence.activities.find(
         (el) => el.name.toLowerCase() == `league of legends` && el.type == 'PLAYING',
     );
+    let leaguePlayerRole = newPresence.member.guild.roles.cache.find(
+        (el) => el.name.toLowerCase() == 'league player',
+    );
     if (isPlayingLeague && !wasPlayingLeague) {
         (
             newPresence.member.guild.channels.cache
                 .filter((el) => el.type == 'GUILD_TEXT')
                 .first() as TextChannel
         ).send(`NOOB STOP PLAYING LEAGUE OF LEGENDS <@${oldPresence.user.id}> `);
-        if (newPresence.member.voice) {
-            newPresence.member.voice.setMute(true, 'League player');
+        if (leaguePlayerRole) {
+            newPresence.member.roles.add(leaguePlayerRole);
+            // newPresence.member.voice.setMute(true, 'League player');
         }
     } else if (!isPlayingLeague && wasPlayingLeague) {
-        if (newPresence.member.voice) {
-            newPresence.member.voice.setMute(false);
+        if (leaguePlayerRole) {
+            newPresence.member.roles.remove(leaguePlayerRole);
         }
     }
 
