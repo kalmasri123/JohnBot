7e68b0edbde3db4954b71df7ff1f94d332e77661
kalmasri123 <kalmasri@me.com>
2022-02-07 12:50:42 -0500

    Update PresenceUpdate.ts

--- src/controllers/PresenceUpdate.ts
+++ src/controllers/PresenceUpdate.ts
@@ -1,10 +1,11 @@
 import { Presence, TextChannel } from 'discord.js';
 const playingLeague = (el) => el.name.toLowerCase() == `league of legends` && el.type == 'PLAYING';
-const leagueRole = (el) => el.name.toLowerCase() == 'league player';
+const findRole = (el) => el.name.toLowerCase() == 'league player';
 export default async function PresenceUpdate(oldPresence: Presence, newPresence: Presence) {
-    let wasPlayingLeague = oldPresence.activities.find(playingLeague);
-    let isPlayingLeague = newPresence.activities.find(playingLeague);
-    let leaguePlayerRole = newPresence.member.guild.roles.cache.find(leagueRole);
+    const wasPlayingLeague = oldPresence.activities.find(playingLeague);
+    const isPlayingLeague = newPresence.activities.find(playingLeague);
+    const leaguePlayerRole = newPresence.member.guild.roles.cache.find(findRole);
+    const member = newPresence.member;
     if (isPlayingLeague && !wasPlayingLeague) {
         (
             newPresence.member.guild.channels.cache
@@ -12,14 +13,12 @@ export default async function PresenceUpdate(oldPresence: Presence, newPresence:
                 .first() as TextChannel
         ).send(`NOOB STOP PLAYING LEAGUE OF LEGENDS <@${oldPresence.user.id}> `);
         if (leaguePlayerRole) {
-            newPresence.member.roles.add(leaguePlayerRole);
+            member.roles.add(leaguePlayerRole);
             // newPresence.member.voice.setMute(true, 'League player');
         }
     } else if (!isPlayingLeague && wasPlayingLeague) {
         if (leaguePlayerRole) {
-            newPresence.member.roles.remove(leaguePlayerRole);
+            member.roles.remove(leaguePlayerRole);
         }
     }
-
-    console.log(isPlayingLeague, wasPlayingLeague);
 }
