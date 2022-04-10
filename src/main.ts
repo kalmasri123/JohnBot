0068e08d9383f51bb600212104ebcc6531b95ea3
kareem <kalmasri@me.com>
2022-04-10 13:51:31 -0400

    minor changes

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
-import 'module-alias/register';
+// import 'module-alias/register';
 
 import { env } from '@util/env';
 import PresenceUpdate from './controllers/PresenceUpdate';
 import { Client, Intents } from 'discord.js';
 import Message from './controllers/Message';
-import '@util/actions'
+import '@util/actions';
 import { GuildScheduledEventPrivacyLevels } from 'discord.js/typings/enums';
-const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 try {
     const client: Client = new Client({
         intents: [
@@ -22,25 +22,32 @@ try {
         },
     });
     const GAMES_TO_PLAY = [
-        'dota 2',
-        'age of empires ii: definitive edition',
-        'age of empires ii (2013)',
-        'age of empires iv',
+        { game: 'Dota 2', points: 200, minPlayTime: 25 * 60 * 1000 },
+        { game: 'Age of Empires II: Definitive Edition', points: 100, minPlayTime: 15 * 60 * 1000 },
+        { game: 'Age of Empires II (2013)', points: 100, minPlayTime: 15 * 60 * 1000 },
+        { game: 'Age of Empires IV', points: 100, minPlayTime: 15 * 60 * 1000 },
+        { game: 'Hell Let Loose', points: 100, minPlayTime: 15 * 60 * 1000 },
+        { game: 'Risk Of Rain 2', points: 100, minPlayTime: 15 * 60 * 1000 },
+        { game: 'Counter-Strike: Global Offensive', points: 10, minPlayTime: 30 * 60 * 1000 },
     ];
-    async function playAllGames(){
-        for(let i = 0; i < GAMES_TO_PLAY.length; i++){
-            await client.user.setActivity({ type: 'PLAYING', name: GAMES_TO_PLAY[i] });
-            await delay(60000 * 60)
+    async function playAllGames() {
+        for (let i = 0; i < GAMES_TO_PLAY.length; i++) {
+            await client.user.setActivity({ type: 'PLAYING', name: GAMES_TO_PLAY[i].game });
+            await delay(GAMES_TO_PLAY[i].minPlayTime + 25000);
+            await delay(2000);
+            
+            await client.user.setActivity(null);
+            await delay(10000);
 
         }
     }
     client.on('ready', async () => {
         console.log('BOT IS READY');
-        await client.user.setActivity(null)
-        playAllGames()
+        await client.user.setActivity(null);
+        playAllGames();
 
         setInterval(function () {
-            playAllGames()
+            playAllGames();
         }, 1000 * 60 * 60 * 24);
     });
     client.login(env.BOT_TOKEN);
