1957b2dd63a46f607c611d90b04719162bca9fdf
Kareem <kareemalmasri9@gmail.com>
2022-06-14 23:00:12 -0700

    removed league of legends codes

--- src/controllers/PresenceUpdate.ts
+++ src/controllers/PresenceUpdate.ts
@@ -2,26 +2,26 @@ import { Presence, TextChannel } from 'discord.js';
 const playingLeague = (el) => el.name.toLowerCase() == `league of legends` && el.type == 'PLAYING';
 const findRole = (el) => el.name.toLowerCase() == 'league player';
 export default async function PresenceUpdate(oldPresence: Presence, newPresence: Presence) {
-    if(!oldPresence || !newPresence) return;
-    console.log(newPresence)
-    const wasPlayingLeague = oldPresence?.activities.find(playingLeague);
-    const isPlayingLeague = newPresence?.activities.find(playingLeague);
+    // if(!oldPresence || !newPresence) return;
+    // console.log(newPresence)
+    // const wasPlayingLeague = oldPresence?.activities.find(playingLeague);
+    // const isPlayingLeague = newPresence?.activities.find(playingLeague);
     
-    const leaguePlayerRole = newPresence.member.guild.roles.cache.find(findRole);
-    const member = newPresence.member;
-    if (isPlayingLeague && !wasPlayingLeague) {
-        (
-            newPresence.member.guild.channels.cache
-                .filter((el) => el.type == 'GUILD_TEXT')
-                .find(el=>el.name=="x-æ-a-12") as TextChannel
-        ).send(`NOOB STOP PLAYING LEAGUE OF LEGENDS <@${oldPresence.user.id}> `);
-        if (leaguePlayerRole) {
-            member.roles.add(leaguePlayerRole);
-            // newPresence.member.voice.setMute(true, 'League player');
-        }
-    } else if (!isPlayingLeague && wasPlayingLeague) {
-        if (leaguePlayerRole) {
-            member.roles.remove(leaguePlayerRole);
-        }
-    }
+    // const leaguePlayerRole = newPresence.member.guild.roles.cache.find(findRole);
+    // const member = newPresence.member;
+    // if (isPlayingLeague && !wasPlayingLeague) {
+    //     (
+    //         newPresence.member.guild.channels.cache
+    //             .filter((el) => el.type == 'GUILD_TEXT')
+    //             .find(el=>el.name=="x-æ-a-12") as TextChannel
+    //     ).send(`NOOB STOP PLAYING LEAGUE OF LEGENDS <@${oldPresence.user.id}> `);
+    //     if (leaguePlayerRole) {
+    //         member.roles.add(leaguePlayerRole);
+    //         // newPresence.member.voice.setMute(true, 'League player');
+    //     }
+    // } else if (!isPlayingLeague && wasPlayingLeague) {
+    //     if (leaguePlayerRole) {
+    //         member.roles.remove(leaguePlayerRole);
+    //     }
+    // }
 }
