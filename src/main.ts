bbd53319ce3cea5513ad498aedbf1a852e990963
Kareem <kareemalmasri9@gmail.com>
2022-07-05 21:15:24 -0700

    init_commit

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import 'module-alias/register';
+// import 'module-alias/register';
 
 import { env } from '@util/env';
 import PresenceUpdate from './controllers/PresenceUpdate';
@@ -6,6 +6,7 @@ import MessageEvent from './controllers/Message';
 
 import { Client, Intents, VoiceChannel } from 'discord.js';
 import '@util/actions';
+import fs from 'fs'
 import { GuildScheduledEventEntityTypes, PrivacyLevels } from 'discord.js/typings/enums';
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 try {
@@ -23,12 +24,10 @@ try {
         },
     });
     async function createImportantEvent() {
-        // const secretChat = await client.guilds.fetch('383034248954773505');
         // const startTime = new Date();
         // startTime.setHours(startTime.getHours() + 1, 0, 0, 0);
         // // const endTime = new Date();
         // // endTime.setHours(startTime.getHours() + 8, 0, 0);
-
         // let event = await secretChat.scheduledEvents.create({
         //     name: 'Do gus mom',
         //     scheduledStartTime: startTime,
@@ -47,7 +46,26 @@ try {
         await client.user.setActivity(null);
         await client.user.setPresence({ status: 'online' });
         //Create Scheduled events
-        await createImportantEvent();
+        const secretChat = await client.guilds.fetch('383034248954773505');
+        setInterval(function () {
+            const sharp = require('sharp');
+            const referenceDate = new Date('07-05-2022');
+            let dayDiff = (Date.now() - referenceDate.getTime()) / (1000 * 60 * 60 * 24);
+            let alpha = 1 - 0.01 * Math.ceil(dayDiff);
+            sharp('palpatine.png')
+                .removeAlpha()
+                .ensureAlpha(alpha)
+                .toBuffer()
+                .then((buff) => {
+                    sharp('john.png')
+                        .composite([{ input: buff }])
+                        .toBuffer()
+                        .then((buff2) => {
+                            secretChat.setIcon(buff2)
+                        });
+                });
+        },5000);
+
         setInterval(createImportantEvent, 1000 * 60 * 60 * 16);
     });
     client.login(env.BOT_TOKEN);
